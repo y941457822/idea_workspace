0f366d0980e454d854e575ef9a6d1e04fd529fad
yinxx <741877449@qq.com>
2018-02-08 11:42:40 +0800

    第一次提交

---
+++ xingxing/src/main/java/com/xingxing/test/test1.java
@@ -0,0 +1,11 @@
+package com.xingxing.test;
+
+/**
+ * Created by Administrator on 2018-01-09.
+ */
+public class test1 {
+    public static void main(String[] args) {
+        System.out.println("PZ8XQPV5I1JF20HK7DGQYDZPD3QOXO090G78SEL96K4DSTI22C0MJ1WASKJQ7XDV7AUDT5D7NXFW4I4KGNJIDCU7FV30M8J99Z27VBVROBGXT3EYCGYLCHZIF9JW9VDA".length() / 4);
+
+    }
+}
